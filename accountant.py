5254080d8c22fa2084ae1392bf574f06b40e004d
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-05-27 22:52:51 +0200

    Poprawiono saldo, utworzono magazyn, poprawiono zakup

--- accountant.py
+++ accountant.py
@@ -9,47 +9,73 @@
 
 import sys
 
+stan_konta = 0
 srodki = 0
-stan_magazyn = 0
+id_produkt = []
 ilosc_magazyn = 0
+magazyn = {"produkt": id_produkt, "stan": ilosc_magazyn}
 dane = []
 
-while True:
-    if sys.argv[1] == "saldo":
-        saldo = str(input())
-        srodki = int(input())       #srodki chyba powinno byc float
-        comment = str(input())
-        dane.append([saldo, srodki, comment])       #insert?
-        print("saldo: {}, komentarz: {}".format(srodki, comment))
 
-    if sys.argv[1] == "sprzedaz":
+
+
+while True:
+    akcja = input().strip()
+    if akcja == "saldo":
+        srodki = int(input())
+        comment = str(input())
+        if stan_konta + srodki < 0:
+            print("error")
+            break
+        dane.append([akcja, srodki, comment])
+        stan_konta += srodki
+        print("środki: {}, komentarz: {}".format(srodki, comment))
+        print(stan_konta)
+        # continue
+
+    akcja = input().strip()
+    if akcja == "zakup":
+        id_produkt = str(input())
+        cena = int(input())
+        sztuk = int(input())
+        if (cena * sztuk) < stan_konta:
+            stan_konta =- cena * sztuk
+            magazyn["produkt"] += id_produkt
+            ilosc_magazyn += sztuk
+            dane.append([akcja, id_produkt, cena, sztuk])
+            print("produkt: {}, cena za szt.: {}, kupiono sztuk: {}".format(id_produkt, cena, sztuk))
+
+        elif cena < 0 or sztuk <= 0:
+            print("Podano nieprawidłowe wartości.")
+            break
+        else:
+            print("Niewystarczające środki na koncie.")
+            break
+        continue
+
+    if akcja == "magazyn":
+        for id_produkt in sys.argv[2:]:
+            if id_produkt in magazyn:
+                stan_magazyn = magazyn[id_produkt]
+            else:
+                stan_magazyn = 0
+            print("{}: ", "{}".format(id_produkt, stan_magazyn))
+            break
+
+    akcja = input().strip()
+    if akcja == "sprzedaz":
         produkt = str(sys.argv[2])
         cena = int(sys.argv[3])
         sztuk = int(sys.argv[4])
         dane.append([input(), produkt, cena, sztuk])
         print("produkt: {}, cena za szt.: {}, sprzedano sztuk: {}".format(produkt, cena, sztuk))
+        continue
 
-    if sys.argv[1] == "zakup":
-        produkt = str(sys.argv[2])
-        cena = int(sys.argv[3])
-        sztuk = int(sys.argv[4])
-        if (cena * sztuk) < srodki:
-            dane.append([input(), produkt, cena, sztuk])
-            srodki -= cena * sztuk
-            stan_magazyn += produkt
-            ilosc_magazyn += sztuk
-            print("produkt: {}, cena za szt.: {}, kupiono sztuk: {}".format(produkt, cena, sztuk))
-        elif cena < 0:
-            print("Nieprawidłowa wartość ceny.")
-            break
-        elif sztuk <= 0:
-            print("Nieprawidłowa ilość sztuk")
-            break
-        else:
-            print("Niewystarczające środki na koncie.")
-            break
-    else:
-        print("ERROR! Podano nieprawidłowe wartości.")
-        break
+    # else:
+        # print("ERROR! Podano nieprawidłowe wartości.")
+        # break
 
-print(dane)
+
+
+
+
