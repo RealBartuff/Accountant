44d2264e701abbcd1998d9e0ef96fd41c3747ad8
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-05-28 23:32:04 +0200

    dodałem przeglad. Saldo, zakup, sprzedaz dzialają w miare poprawnie, dodaje sie stan magazynowy

--- accountant.py
+++ accountant.py
@@ -10,72 +10,88 @@
 import sys
 
 stan_konta = 0
-srodki = 0
-id_produkt = []
+zmiana = 0
+konto = {"saldo": stan_konta}
+id_produkt = str()
 ilosc_magazyn = 0
-magazyn = {"produkt": id_produkt, "stan": ilosc_magazyn}
+magazyn = []
 dane = []
-
 
 
 
 while True:
     akcja = input().strip()
     if akcja == "saldo":
-        srodki = int(input())
+        zmiana = int(input())
         comment = str(input())
-        if stan_konta + srodki < 0:
+        if stan_konta + zmiana < 0:
             print("error")
             break
-        dane.append([akcja, srodki, comment])
-        stan_konta += srodki
-        print("środki: {}, komentarz: {}".format(srodki, comment))
-        print(stan_konta)
+        stan_konta += zmiana
+        konto["saldo"] =+ stan_konta
+        dane.append([akcja, zmiana, comment])
+        print(akcja)
+        print("środki: {}, komentarz: {}".format(zmiana, comment))
         # continue
-
-    akcja = input().strip()
-    if akcja == "zakup":
+    elif akcja == "zakup":
         id_produkt = str(input())
         cena = int(input())
         sztuk = int(input())
         if (cena * sztuk) < stan_konta:
             stan_konta =- cena * sztuk
-            magazyn["produkt"] += id_produkt
             ilosc_magazyn += sztuk
+            konto["saldo"] += stan_konta
+            magazyn.append(id_produkt)
+            magazyn.append(ilosc_magazyn)
             dane.append([akcja, id_produkt, cena, sztuk])
+            print(akcja)
             print("produkt: {}, cena za szt.: {}, kupiono sztuk: {}".format(id_produkt, cena, sztuk))
-
         elif cena < 0 or sztuk <= 0:
             print("Podano nieprawidłowe wartości.")
             break
         else:
             print("Niewystarczające środki na koncie.")
             break
-        continue
-
-    if akcja == "magazyn":
-        for id_produkt in sys.argv[2:]:
-            if id_produkt in magazyn:
-                stan_magazyn = magazyn[id_produkt]
-            else:
-                stan_magazyn = 0
-            print("{}: ", "{}".format(id_produkt, stan_magazyn))
-            break
-
-    akcja = input().strip()
-    if akcja == "sprzedaz":
-        produkt = str(sys.argv[2])
-        cena = int(sys.argv[3])
-        sztuk = int(sys.argv[4])
-        dane.append([input(), produkt, cena, sztuk])
-        print("produkt: {}, cena za szt.: {}, sprzedano sztuk: {}".format(produkt, cena, sztuk))
-        continue
+        # continue
+    elif akcja == "sprzedaz":
+        id_produkt = str(input())
+        cena = int(input())
+        sztuk = int(input())
+        if id_produkt in magazyn and sztuk <= ilosc_magazyn:
+            magazyn.remove(id_produkt)
+            magazyn.remove(ilosc_magazyn)
+            stan_konta =+ cena * sztuk
+            konto["saldo"] += stan_konta
+            dane.append([akcja, id_produkt, cena, sztuk])
+        print(akcja)
+        print("produkt: {}, cena za szt.: {}, sprzedano sztuk: {}".format(id_produkt, cena, sztuk))
+        # continue
+    elif akcja == "przeglad":
+        for wpis in dane:
+            for element in wpis:
+                print(element)
+        break
+    elif akcja == "stop":
+        print("Koniec programu.")
+        break
+    #
+    # elif akcja == "magazyn":
+    #     for id_produkt in sys.argv[2:]:
+    #         if id_produkt in magazyn:
+    #             stan_magazyn = magazyn[id_produkt]
+    #         else:
+    #             stan_magazyn = 0
+    #         print("{}: ", "{}".format(id_produkt, stan_magazyn))
+    #         break
+    #
 
     # else:
         # print("ERROR! Podano nieprawidłowe wartości.")
         # break
 
 
-
-
-
+    # akcja == sys.argv[1]
+    # if akcja == "przeglad":
+    #     for wpis in dane:
+    #         for element in wpis:
+    #             print(element)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
-lista = [input()]
-
-print(lista)
+a = 1
+print(id(a))
+a = a+1
+print(id(a))
