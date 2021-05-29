aba689c8caca1496e306986e2ff36e933cbeaad4
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-05-29 23:40:29 +0200

    skróciłem kod na test.py dzieki funkcji or

--- test.py
+++ test.py
@@ -1,94 +1,65 @@
 
-# import sys
+import sys
 
 stan_konta = 0
 zmiana = 0
 konto = {"saldo": stan_konta}
-id_produkt = str()
-cena = 0
-sztuk = 0
-ilosc_magazyn = 0
 magazyn = []
 dane = []
 
 
 
 while True:
-    akcja = input().strip()
+    akcja = input().strip() or sys.argv[1]
     if akcja == "saldo":
-        zmiana = int(input())
-        comment = str(input())
+        zmiana = int(input()) or sys.argv[2]
+        comment = str(input()) or sys.argv[3]
         if stan_konta + zmiana < 0:
             print("error")
             break
         stan_konta += zmiana
         konto["saldo"] =+ stan_konta
         dane.append([akcja, zmiana, comment])
-        print(konto)
 
     elif akcja == "zakup":
-        id_produkt = str(input())
-        cena = int(input())
-        sztuk = int(input())
-        if cena < 0 or sztuk <= 0:
+        id_produkt = str(input()) or sys.argv[2]
+        cena = int(input()) or sys.argv[3]
+        sztuk = int(input()) or sys.argv[4]
+        if (cena * sztuk) < konto["saldo"]:
+            stan_konta =- cena * sztuk
+            konto["saldo"] += stan_konta
+            magazyn.append([id_produkt, sztuk])
+            dane.append([akcja, id_produkt, cena, sztuk])
+        elif cena < 0 or sztuk <= 0:
             print("Podano nieprawidłowe wartości.")
             break
-        elif (cena * sztuk) < stan_konta:
-            stan_konta =- cena * sztuk
-            ilosc_magazyn += sztuk
-            konto["saldo"] += stan_konta
-            magazyn.append(id_produkt)
-            magazyn.append(ilosc_magazyn)
-            dane.append([akcja, id_produkt, cena, sztuk])
-        print(konto["saldo"])
-        print(dane)
 
     elif akcja == "sprzedaz":
-        id_produkt = str(input())
-        cena = int(input())
-        sztuk = int(input())
-        if id_produkt in magazyn and sztuk <= ilosc_magazyn:
-            magazyn.remove(id_produkt)
-            magazyn.remove(ilosc_magazyn)
+        id_produkt = str(input()) or sys.argv[2]
+        cena = int(input()) or sys.argv[3]
+        sztuk = int(input()) or sys.argv[4]
+        if [id_produkt, sztuk] in magazyn:
+            magazyn.remove([id_produkt, sztuk])
             stan_konta =+ cena * sztuk
             konto["saldo"] += stan_konta
             dane.append([akcja, id_produkt, cena, sztuk])
-        print(konto)
-        
-# while True:
-#     if sys.argv[1] == "saldo":
-#         print(konto["saldo"])
-#         pass
-#
-#     elif sys.argv[1] == "zakup":
-#         dane.append([sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]])
-#         for wpis in dane:
-#             for element in wpis:
-#                 print(element)
-#         print("stop")
-#
-#     elif sys.argv[1] == "sprzedaz":
-#         dane.append([sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]])
-#         for wpis in dane:
-#             for element in wpis:
-#                 print(element)
-#         print("stop")
-#
-#     elif sys.argv[1] == "konto":
-#         print(konto["saldo"])
-#         break
-#
-#     elif sys.argv[1] == "przeglad":
-#         for wpis in dane:
-#             for element in wpis:
-#                 print(element)
-#         break
-#
+
+    elif sys.argv[1] == "saldo":
+        print(konto["saldo"])
+        break
+
+    elif sys.argv[1] == "konto":
+        print(konto["saldo"])
+        break
+    #
+    # elif sys.argv[1] == "przeglad":
+    #     for wpis in dane:
+    #         for element in wpis:
+    #             print(element)
+    #     break
+
     elif akcja == "stop":
-        # for wpis in dane:
-        #     for element in wpis:
-        #                 print(element)
-        print("Koniec programu.")
+        print("stop")
         break
 
     #
@@ -105,10 +76,3 @@ while True:
     # else:
         # print("ERROR! Podano nieprawidłowe wartości.")
         # break
-
-
-    # akcja == sys.argv[1]
-    # if akcja == "przeglad":
-    #     for wpis in dane:
-    #         for element in wpis:
-    #             print(element)
