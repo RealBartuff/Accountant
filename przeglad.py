ce9a81438fd83376fb17d1c8bd7e0f90b8e3f5d2
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-07-22 11:47:20 +0200

    przeglad dziala na dodanej metodzie w klasie manager

--- przeglad.py
+++ przeglad.py
@@ -1,8 +1,34 @@
 import sys
 
-from acclibrary import Magazyn
+from manager import Reader, Manager
 
-magazyn = Magazyn()
+reader = Reader("in.txt")
+manager = Manager(reader)
 
-magazyn.wczytaj(sys.argv[1])
-magazyn.przeglad(sys.argv[2], sys.argv[3])
+
+@manager.action("saldo", 2)
+def saldo(manager, rows):
+    price = float(rows[0])
+    manager.modify_account(price)
+
+
+@manager.action("zakup", 3)
+def zakup(manager, rows):
+    name = rows[0]
+    price = float(rows[1])
+    qty = float(rows[2])
+    manager.modify_account(-price*qty)
+    manager.modify_stock(name, qty)
+
+
+@manager.action("sprzedaz", 3)
+def zakup(manager, rows):
+    name = rows[0]
+    price = float(rows[1])
+    qty = float(rows[2])
+    manager.modify_account(price*qty)
+    manager.modify_stock(name, -qty)
+
+
+manager.process()
+manager.review(sys.argv[1], sys.argv[2])
